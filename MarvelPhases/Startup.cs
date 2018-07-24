384c82ad33bf891a2e15672d243702278cad4de1
Tony Piccirilli <anpicc01@louisville.edu>
2018-07-24 16:42:25 -0400

    Add project files.

---
+++ MarvelPhases/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarvelPhases.Startup))]
+namespace MarvelPhases
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
